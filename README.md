17d6a346a82416e8f26c1bbebbc7e2a8feca0bf1
snowytech <drew@Drews-MBP.home>
2017-03-29 15:12:16 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+qBMI
+===============
+
+qBMI (quick BMI) is a very simple, and BMI calculator. It will help calculate your Body Mass Index using height and weight. Calculates in metric/kilograms, centimeters/kilograms, inches/pounds, and feet/inches/pounds. Great to help you stay healthy and fit! 
+
+Committing old code that has never been in source control.  Code was originally written circa 2012-2013 as an HTML5/Cordova application for Android.  Last updated January 2016 to utilize Boostrap CSS.  This is the browser version and doesn't include the Android project.
+
+Demo:  http://www.snowytech.com/data/qBMI2.0/
+
+App can still be found on Google Play Store:  https://play.google.com/store/apps/details?id=com.snowytech.qbmi
+
+
+Required
+--------
+JQuery, JQuery Mobile, Bootstrap, Web Server ( Apache, MAMP, WAMP, etc.. )
+
+
+Usage
+-----
+
+Run on a webserver.
+
+
+Website
+-------
+http://snowytech.com
+
+
+License
+-------
+MIT License
+
+Copyright (c) 2017 Drew D. Lenhart
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ javascript/main.js
@@ -0,0 +1,180 @@
+//snowytech.com  
+//v2.0
+//December 2015
+
+
+var slide = {
+	transition : "pop"
+};
+var answerOne = "#answerPop";
+var invalidPop = "#invalid";
+
+// Begin main bmiE function ---------------------------------------------
+var bmiE = function() {
+    var x;
+    var y;
+    x = document.getElementById('height');
+    y = document.getElementById('weight');
+    z = document.getElementById('feet');
+    za = document.getElementById('inches');
+    var xSelect = selector.options[selector.options.selectedIndex].value;
+
+    // Form validation. If forms accurate invoke isValid function to calculate.
+    if (xSelect === "eng2") {
+        if (isNaN(z.value && za.value)) {
+            $.mobile.changePage(invalidPop, slide);
+            return false
+        } else if (z.value === "" || z.value === null) {
+            $.mobile.changePage(invalidPop, slide);
+            return false
+        } else if (za.value === "" || za.value === null) {
+            $.mobile.changePage(invalidPop, slide);
+            return false
+        } else if (y.value === "" || y.value === null) {
+            $.mobile.changePage(invalidPop, slide);
+            return false
+        } else {
+            isValid();
+        }
+
+    }
+    else if (xSelect === "eng") {
+        validate(x, y);
+    } else if (xSelect === "metric") {
+        validate(x, y);
+    } else if (xSelect === "metric2") {
+        validate(x, y);
+    }
+
+	// Validate function---------------------------------------
+	function validate(x, y) {
+		if (isNaN(x.value && y.value)) {
+			$.mobile.changePage(invalidPop, slide);
+			return false
+		} else if (x.value === "" || x.value === null) {
+			$.mobile.changePage(invalidPop, slide);
+			return false
+		} else if (y.value === "" || y.value === null) {
+			$.mobile.changePage(invalidPop, slide);
+			return false
+		} else {
+			// invoke inValid() and perform calculations
+			isValid();
+		}
+	}
+
+	// isValid function--if valid, calculate numbers depending on which
+	// selection --------
+	function isValid() {
+		if (xSelect === "metric") {
+			answer = y.value / Math.pow(x.value, 2);
+
+		}
+		if (xSelect === "metric2") {
+			answer = y.value / Math.pow((x.value / 100), 2);
+
+		}
+		if (xSelect === "eng") {
+			answer = [ y.value / Math.pow(x.value, 2) ] * 703;
+
+		}
+		if (xSelect === "eng2") {
+			getinVal = parseFloat(za.value);
+			getInches = z.value * 12 + getinVal;
+			answer = [ y.value / Math.pow(getInches, 2) ] * 703;
+
+		}
+
+		// Invoke popup and display output
+		$.mobile.changePage(answerOne, slide);
+		document.getElementById('answer').innerHTML = Math.round(answer * 100) / 100;
+	}
+
+	if (answer <= 15.99) {
+		document.getElementById('answerOut').innerHTML = "Severe Thinness";
+		return false
+	} else if (answer >= 16.00 && answer <= 16.99) {
+		document.getElementById('answerOut').innerHTML = "Moderate Thinness";
+		return false
+	} else if (answer >= 17.00 && answer <= 18.49) {
+		document.getElementById('answerOut').innerHTML = "Mild Thinness";
+		return false
+	} else if (answer >= 18.50 && answer <= 24.99) {
+		document.getElementById('answerOut').innerHTML = "Normal Weight";
+		return false
+	} else if (answer >= 25.00 && answer <= 29.99) {
+		document.getElementById('answerOut').innerHTML = "Overweight";
+		return false
+	} else if (answer >= 30.00 && answer <= 34.99) {
+		document.getElementById('answerOut').innerHTML = "Obese Class I";
+		return false
+	} else if (answer >= 35.00 && answer <= 39.99) {
+		document.getElementById('answerOut').innerHTML = "Obese Class II";
+		return false
+	} else if (answer >= 40.0) {
+		document.getElementById('answerOut').innerHTML = "Obese Class III";
+		return false
+	}
+
+}
+// end bmiE() function ------------------------------------------------------
+
+// -- checkV -- Hides and displays fields depending on selection--
+function checkV(val) {
+	if (val === "eng2") {
+		dispFeet();
+		dispInch();
+		document.getElementById('height').style.display = 'none';
+		document.getElementById('heightlbl').style.display = 'none';
+	} else if (val === "eng") {
+		dispHei();
+		hideEng();
+	} else if (val === "metric") {
+		dispHei();
+		hideEng();
+	} else if (val === "metric2") {
+		dispHei();
+		hideEng();
+	} else {
+		hideEng();
+	}
+
+}
+
+function dispFeet() {
+	document.getElementById('feetlbl').style.display = 'block';
+    document.getElementById('hideFeet').style.display = 'block';
+}
+function dispInch() {
+	document.getElementById('inchlbl').style.display = 'block';
+    document.getElementById('hideInch').style.display = 'block';
+}
+function hideEng() {
+    document.getElementById('hideFeet').style.display = 'none';
+    document.getElementById('hideInch').style.display = 'none';
+}
+function dispHei(){
+	document.getElementById('height').style.display = 'block';
+	document.getElementById('heightlbl').style.display = 'block';
+}
+
+//Refreshes fields when selecting different mode------------------
+function refresh(){
+	document.getElementById('height').value = "";
+	document.getElementById('weight').value = "";
+	document.getElementById('feet').value = "";
+	document.getElementById('inches').value = "";
+}
+
+// Button click invokes main BMI function
+$(document).ready(function() {
+
+	$('#calcuBMI').tap(function() {
+		bmiE();
+	});
+
+	$('#reSetButton').tap(function() {
+		refresh();
+	});
+
+});
